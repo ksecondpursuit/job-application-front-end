072530d49d937d48845305cfa8a84248fe383473
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-07-18 17:26:02 -0400

    API Fetch Util Update

---
+++ src/utils/api.js
@@ -0,0 +1,13 @@
+const API = import.meta.env.VITE_API_URL;
+
+export const fetchAPI = async (endpoint, options = {}) => {
+  try {
+    const response = await fetch(`${API}${endpoint}`, options);
+    if (!response.ok)
+      throw new Error(`Error: ${response.statusText}`);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};

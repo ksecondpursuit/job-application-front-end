748ac06234c49dbe1c9ce20308e46c67a11ef967
Kenny <ksecond@pursuit.org>
2024-07-18 16:43:50 -0400

    updated

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 // main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from '/src/App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+  } from 'react-router-dom';
+  import EditApplicationForm from './Pages/EditApplicationForm';
+  import FourOFour from './Pages/FourOFour';
+  import Home from './Pages/Home';
+  import ApplicationsIndex from './Pages/ApplicationsIndex';
+  import NewApplicationForm from './Pages/NewApplicationForm';
+  import ShowApplication from './Pages/ShowApplication';
+  import NavBar from './Components/NavBar'; 
+  

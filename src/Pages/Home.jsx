158251cf164e3b0f6101b599559cc3b7841b7b35
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-07-18 17:22:01 -0400

    Home Page Update

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className='Home'>
+      <h2>Welcome to the Job Application Tracker!</h2>
+      <h3>Keep track of your job applications</h3>
+    </div>
+  );
+}
+
+export default Home;

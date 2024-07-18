646feab87c047b1f2761943809619b16acddc43f
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-07-18 17:27:13 -0400

    App Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
 // src/App.jsx
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-  } from 'react-router-dom';
-  import EditApplicationForm from './Pages/EditApplicationForm';
-  import FourOFour from './Pages/FourOFour';
-  import Home from './Pages/Home';
-  import ApplicationsIndex from './Pages/ApplicationsIndex';
-  import NewApplicationForm from './Pages/NewApplicationForm';
-  import ShowApplication from './Pages/ShowApplication';
-  import NavBar from './Components/NavBar'; 
-  
-  function App() {
-    return (
-      <div className='App'>
-        <Router>
-          <NavBar /> 
-          <main>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/applications' element={<ApplicationsIndex />} />
-              <Route path='/applications/new' element={<NewApplicationForm />} />
-              <Route path='/applications/:id' element={<ShowApplication />} />
-              <Route path='/applications/:id/edit' element={<EditApplicationForm />} />
-              <Route path='*' element={<FourOFour />} />
-            </Routes>
-          </main>
-        </Router>
-      </div>
-    );
-  }
-  
-  export default App;
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Edit from './Pages/Edit';
+import FourOFour from './Pages/FourOFour';
+import Home from './Pages/Home';
+import Index from './Pages/Index';
+import New from './Pages/New';
+import Show from './Pages/Show';
+import NavBar from './Components/NavBar';
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/applications' element={<Index />} />
+            <Route path='/applications/new' element={<New />} />
+            <Route path='/applications/:id' element={<Show />} />
+            <Route path='/applications/:id/edit' element={<Edit />} />
+            <Route path='*' element={<FourOFour />} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  );
+}
+
+export default App;

e2ae89b116bd5a78b6297345167a5367592cab0f
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-07-18 17:22:46 -0400

    Index Page Update

---
+++ src/Pages/Index.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import Application from '../Components/Application';
+import { fetchAPI } from '../utils/api';
+
+function Index() {
+  const [applications, setApplications] = useState([]);
+
+  useEffect(() => {
+    fetchAPI(`/applications`)
+      .then(setApplications)
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div className='Index'>
+      <h2>Applications</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Position</th>
+            <th>Company</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {applications.map((application) => (
+            <Application
+              key={application.id}
+              application={application}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Index;

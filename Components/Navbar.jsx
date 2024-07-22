fa0ccb2f1c86b31fca5abea6debb35f4374d718a
jmoncayo-pursuit <jmoncayo@pursuit.org>
2024-07-22 18:58:13 -0400

    feat(NavBar): Add application counter

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,8 +1,25 @@
 // Components/NavBar.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { fetchAPI } from '../src/utils/api'; // Import fetchAPI
 
 function NavBar() {
+  const [applicationCount, setApplicationCount] = useState(0);
+
+  useEffect(() => {
+    // Fetch the number of applications when the component mounts
+    const fetchApplicationCount = async () => {
+      try {
+        const data = await fetchAPI('/applications');
+        setApplicationCount(data.length);
+      } catch (error) {
+        console.error('Error fetching application count:', error);
+      }
+    };
+
+    fetchApplicationCount();
+  }, []);
+
   return (
     <nav>
       <h1>Pursuit's Ultimate Job Tracker</h1>
@@ -11,12 +28,13 @@ function NavBar() {
           <Link to='/'>Home</Link>
         </li>
         <li>
-          <Link to='/applications'>Applications</Link>
+          <Link to='/applications'>
+            Applications ({applicationCount})
+          </Link>
         </li>
         <li>
           <Link to='/applications/new'>New Application</Link>
         </li>
- 
       </ul>
     </nav>
   );
